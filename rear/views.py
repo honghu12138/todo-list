cac50b1f98e1319b62a845dc9d2ed7a62591ec34
weixiongwang <weixiong.wang@gocloudio.com>
2016-10-14 12:29:03 +0800

    todos version 3

--- rear/views.py
+++ rear/views.py
@@ -1,13 +1,13 @@
 # -*- encoding: utf-8 -*-
 from django.shortcuts import render
 from models import Todo 
-
-
-
-
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 # Create your views here.
+@csrf_exempt
 def home(request):
+    print("view goes here")
     if request.method == "POST":
         action = request.POST.get('action')
         if action == "add":
@@ -51,10 +51,6 @@ def home(request):
         })
     
 
-
-
-
-
 def count(todos):
     number = 0
     for todo in todos:
@@ -64,6 +60,90 @@ def count(todos):
             number = number  
 
     return  number
+
+def get_todo(todos):
+    import json
+    todo = dict(
+        content='todo',
+        id=1,
+        completed=True
+    )
+    return HttpResponse(json.dumps(todo))
+
+@csrf_exempt
+def todo(request):
+    import json
+    if request.method == 'POST':
+        text = json.loads(request.body)
+        # imput by jason
+        todo = Todo(content=text.get("content"),state=False)
+        todo.save() 
+        id = todo.id
+        value = todo.content
+        todo = dict(
+            content=value,
+            id=id,
+            state=False
+        )
+        response = HttpResponse(json.dumps(todo))
+        #回复至jason网页
+        response['content-type'] = 'application/json'
+        return response
+   
+    elif request.method == 'GET':
+         filter = request.GET.get('filter')
+         if filter == 'completed':
+               todos = Todo.objects.filter(state=True)
+         elif filter == 'active':
+             todos = Todo.objects.filter(state=False)
+         else:
+             todos = Todo.objects.all() 
+         list=[ ]
+         for todo in todos:
+             todo = dict(
+                content=todo.content,
+                id=todo.id,
+                completed=todo.state
+             )
+             list.insert(0,todo)
+             #
+         response = HttpResponse(json.dumps(list))
+         response['content-type'] = 'application/json'
+         return response
+    else:
+        return HttpResponse("",status=405)
+
+
+@csrf_exempt
+def edit_todo(request, id):
+    if request.method == 'DELETE':
+        try:   
+            todo = Todo.objects.get(id=id)
+            todo.delete()
+            response = HttpResponse("",status=200)
+            return response
+        except Todo.DoesNotExist:
+            return HttpResponse("", status=404)
+
+    elif request.method == 'PUT':
+          import json
+          try:
+              todo = Todo.objects.get(id=id)
+              text = json.loads(request.body)
+              todo.content = text.get('content')
+              id = todo.id
+              value = todo.content
+              todo = dict(
+                  content=todo.content,
+                  id=todo.id,
+                  state=todo.state
+                  ) 
+              response = HttpResponse(json.dumps(todo))
+              response['content-type'] = 'application/json'  
+              return response
+          except Todo.DoesNotExist:
+              return HttpResponse("",status=405)
+
 
 
     
--- todos/urls.py
+++ todos/urls.py
@@ -8,8 +8,10 @@ from rear import views as front_views
 #]
 
 urlpatterns = [
-    url(r'^',front_views.home, name='home'),
+    #url(r'^',front_views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/todos',front_views.todo),
+    url(r'^api/todo/(?P<id>\d+)$',front_views.edit_todo),
     #url(r'^todochange',front_views.todochange),
-    url(r'^$',front_views.home)
+    #url(r'^$',front_views.home)
 ]
